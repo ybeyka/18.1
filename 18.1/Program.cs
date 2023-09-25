2a960cab261f0d59a6d3f2918249eaa996d311d3
ybey <ybeyky@gmail.com>
2023-09-26 00:18:52 +0300

    Добавьте файлы проекта.

---
+++ 18.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть арифметичний вираз:");
+        string expression = Console.ReadLine();
+
+        try
+        {
+            object computationResult = new DataTable().Compute(expression, null);
+            double result = Convert.ToDouble(computationResult);
+
+            if (double.IsInfinity(result) || double.IsNaN(result))
+            {
+                Console.WriteLine("Помилка: ділення на 0.");
+            }
+            else
+            {
+                Console.WriteLine($"Результат: {result}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Помилка: {e.Message}");
+        }
+    }
+}
